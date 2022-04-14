fec19d0e83880480a9bb4228438ed5decce2981f
Shuhuai Cao <csh980717@outlook.com>
2022-04-13 20:48:31 -0400

    Implement article fragment

--- app/src/main/java/tech/tennoji/igncodefoo/article/ArticleFragment.kt
+++ app/src/main/java/tech/tennoji/igncodefoo/article/ArticleFragment.kt
@@ -1,10 +1,14 @@
 package tech.tennoji.igncodefoo.article
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import tech.tennoji.igncodefoo.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,6 +25,11 @@ class ArticleFragment : Fragment() {
     private var param1: Int? = null
     private var param2: Int? = null
 
+    private val viewModel: ArticleViewModel by lazy {
+        val factory = ArticleViewModelFactory()
+        ViewModelProvider(this, factory).get(ArticleViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +42,29 @@ class ArticleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getArticleData(param1!!, param2!!)
+        val adapter = ArticleAdapter(ArticleListener { slug -> viewModel.onCardClick(slug) })
+        val recyclerView = view.findViewById<RecyclerView>(R.id.article_recycler_view)
+        if (recyclerView != null) {
+            recyclerView.adapter = adapter
+        }
+        viewModel.articleList.observe(viewLifecycleOwner) {
+            adapter.submitList(it.data)
+        }
+
+        viewModel.openLink.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                Log.i("article", it)
+            }
+        })
     }
 
     companion object {
