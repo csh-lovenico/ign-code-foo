298180cde729ea19b61b96bd756799becaa32013
Shuhuai Cao <csh980717@outlook.com>
2022-04-14 11:17:07 -0400

    Video fragment

--- app/src/main/java/tech/tennoji/igncodefoo/video/VideoFragment.kt
+++ app/src/main/java/tech/tennoji/igncodefoo/video/VideoFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import tech.tennoji.igncodefoo.R
+import tech.tennoji.igncodefoo.databinding.FragmentVideoBinding
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_START_INDEX = "startIndex"
@@ -20,6 +25,11 @@ class VideoFragment : Fragment() {
     private var param1: Int? = null
     private var param2: Int? = null
 
+    private val viewModel: VideoViewModel by lazy {
+        val factory = VideoViewModelFactory()
+        ViewModelProvider(this, factory).get(VideoViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -31,9 +41,38 @@ class VideoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentVideoBinding>(
+            inflater,
+            R.layout.fragment_video,
+            container,
+            false
+        )
+        binding.lifecycleOwner = this
+        val adapter = VideoAdapter(VideoListener { slug -> viewModel.onCardClick(slug) })
+        val recyclerView = binding.videoRecyclerView
+        recyclerView.adapter = adapter
+        viewModel.videoList.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+
+        viewModel.openLink.observe(viewLifecycleOwner) {
+            it?.let {
+                val bundle = Bundle()
+                bundle.putString("url", "https://ign.com/videos/$it")
+                findNavController().navigate(R.id.action_mainFragment_to_webViewFragment, bundle)
+                viewModel.onOpenLinkComplete()
+            }
+        }
+        viewModel.error.observe(viewLifecycleOwner) {
+            if (it) {
+                Toast.makeText(context, "Network error", Toast.LENGTH_LONG).show()
+                viewModel.onErrorToastComplete()
+            }
+        }
+        viewModel.getVideoData(param1!!, param2!!)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video, container, false)
+        return binding.root
     }
 
     companion object {
